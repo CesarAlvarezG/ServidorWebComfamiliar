2d43b0c8d4f3c112f20e6f71fdd77c95197486a1
CesarAlvarezG <ing.cesaralvarezg@gmail.com>
2023-04-03 08:41:59 -0500

    Actualización de las vistas para incluir logos institucionales

--- resources/views/Sitio/AllSistems.blade.php
+++ resources/views/Sitio/AllSistems.blade.php
@@ -38,18 +38,18 @@
                             <div class="widget widget-default widget-carousel">
                                 <div class="owl-carousel" id="owl-example">
                                     <div>                                    
-                                        <div class="widget-title">Total Visitors</div>                                                                        
-                                        <div class="widget-subtitle">27/08/2022 15:23</div>
-                                        <div class="widget-int">3,548</div>
+                                        <div class="widget-title">Sistemas instalados</div>                                                                        
+                                        <div class="widget-subtitle">Operativos</div>
+                                        <div class="widget-int">20</div>
                                     </div>
                                     <div>                                    
-                                        <div class="widget-title">Returned</div>
-                                        <div class="widget-subtitle">Visitors</div>
+                                        <div class="widget-title">Datos almacenados</div>
+                                        <div class="widget-subtitle"></div>
                                         <div class="widget-int">1,695</div>
                                     </div>
                                     <div>                                    
-                                        <div class="widget-title">New</div>
-                                        <div class="widget-subtitle">Visitors</div>
+                                        <div class="widget-title">Reportes</div>
+                                        <div class="widget-subtitle">Descargados</div>
                                         <div class="widget-int">1,977</div>
                                     </div>
                                 </div>                            
@@ -69,8 +69,8 @@
                                 </div>                             
                                 <div class="widget-data">
                                     <div class="widget-int num-count">48</div>
-                                    <div class="widget-title">New messages</div>
-                                    <div class="widget-subtitle">In your mailbox</div>
+                                    <div class="widget-title">Solicitudes</div>
+                                    <div class="widget-subtitle">En Proceso</div>
                                 </div>      
                                 <div class="widget-controls">                                
                                     <a href="#" class="widget-control-right widget-remove" data-toggle="tooltip" data-placement="top" title="Remove Widget"><span class="fa fa-times"></span></a>
@@ -88,8 +88,8 @@
                                 </div>
                                 <div class="widget-data">
                                     <div class="widget-int num-count">375</div>
-                                    <div class="widget-title">Registred users</div>
-                                    <div class="widget-subtitle">On your website</div>
+                                    <div class="widget-title">Usuarios Registrados</div>
+                                    <div class="widget-subtitle">En el sistema</div>
                                 </div>
                                 <div class="widget-controls">                                
                                     <a href="#" class="widget-control-right widget-remove" data-toggle="tooltip" data-placement="top" title="Remove Widget"><span class="fa fa-times"></span></a>
@@ -131,8 +131,8 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="panel-title-box">
-                                        <h3>Sales</h3>
-                                        <span>Sales activity by period you selected</span>
+                                        <h3>Sistemas</h3>
+                                        <span>Actividad de los sistemas</span>
                                     </div>                                     
                                     <ul class="panel-controls panel-controls-title">                                        
                                         <li>
@@ -148,34 +148,34 @@
                                     <div class="row stacked">
                                         <div class="col-md-4">                                            
                                             <div class="progress-list">                                               
-                                                <div class="pull-left"><strong>In Queue</strong></div>
-                                                <div class="pull-right">75%</div>                                                
+                                                <div class="pull-left"><strong>Instalados</strong></div>
+                                                <div class="pull-right">100%</div>                                                
                                                 <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 75%;">75%</div>
+                                                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
                                                 </div>
                                             </div>
                                             <div class="progress-list">                                               
-                                                <div class="pull-left"><strong>Shipped Products</strong></div>
-                                                <div class="pull-right">450/500</div>                                                
+                                                <div class="pull-left"><strong>Datos almacenados</strong></div>
+                                                <div class="pull-right">1695/10000</div>                                                
                                                 <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 90%;">90%</div>
+                                                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 16%;">16%</div>
                                                 </div>
                                             </div>
                                             <div class="progress-list">                                               
-                                                <div class="pull-left"><strong class="text-danger">Returned Products</strong></div>
-                                                <div class="pull-right">25/500</div>                                                
+                                                <div class="pull-left"><strong class="text-danger">Reportes</strong></div>
+                                                <div class="pull-right">1977/500000</div>                                                
                                                 <div class="progress progress-small progress-striped active">
-                                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 5%;">5%</div>
+                                                    <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 1%;">1%</div>
                                                 </div>
                                             </div>
                                             <div class="progress-list">                                               
-                                                <div class="pull-left"><strong class="text-warning">Progress Today</strong></div>
+                                                <div class="pull-left"><strong class="text-warning">Progreso del día</strong></div>
                                                 <div class="pull-right">75/150</div>                                                
                                                 <div class="progress progress-small progress-striped active">
                                                     <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div>
                                                 </div>
                                             </div>
-                                            <p><span class="fa fa-warning"></span> Data update in end of each hour. You can update it manual by pressign update button</p>
+                                            <p><span class="fa fa-warning"></span> Los datos deben ser actualizados manualmente por medio del boton actualizar</p>
                                         </div>
                                         <div class="col-md-8">
                                             <div id="dashboard-map-seles" style="width: 100%; height: 225px"></div>
@@ -192,8 +192,8 @@
                             <div class="panel panel-default">
                                 <div class="panel-heading">
                                     <div class="panel-title-box">
-                                        <h3>Projects</h3>
-                                        <span>Projects activity</span>
+                                        <h3>Proyectos</h3>
+                                        <span>Actividad  de los Proyectos</span>
                                     </div>                                    
                                     <ul class="panel-controls" style="margin-top: 2px;">
                                         <li><a href="#" class="panel-fullscreen"><span class="fa fa-expand"></span></a></li>
@@ -213,9 +213,9 @@
                                         <table class="table table-condensed table-bordered table-striped">
                                             <thead>
                                                 <tr>
-                                                    <th width="50%">Project</th>
-                                                    <th width="20%">Status</th>
-                                                    <th width="30%">Activity</th>
+                                                    <th width="50%">Proyecto</th>
+                                                    <th width="20%">Estatus</th>
+                                                    <th width="30%">Actividad</th>
                                                 </tr>
                                             </thead>
                                             <tbody>
@@ -296,7 +296,7 @@
 
 @section('scriptsTemplate')
      <!-- START THIS PAGE PLUGINS-->        
-     <script type='text/javascript' src='{{asset('js/plugins/icheck/icheck.min.js')}}'></script>        
+     <script type='text/javascript' src="{{asset('js/plugins/icheck/icheck.min.js')}}"></script>        
         <script type="text/javascript" src="{{asset('js/plugins/mcustomscrollbar/jquery.mCustomScrollbar.min.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/plugins/scrolltotop/scrolltopcontrol.js')}}"></script>
         
@@ -304,9 +304,9 @@
         <script type="text/javascript" src="{{asset('js/plugins/morris/morris.min.js')}}"></script>       
         <script type="text/javascript" src="{{asset('js/plugins/rickshaw/d3.v3.js')}}"></script>
         <script type="text/javascript" src="{{asset('js/plugins/rickshaw/rickshaw.min.js')}}"></script>
-        <script type='text/javascript' src='{{asset('js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}'></script>
-        <script type='text/javascript' src='{{asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}'></script>                
-        <script type='text/javascript' src='{{asset('js/plugins/bootstrap/bootstrap-datepicker.js')}}'></script>                
+        <script type='text/javascript' src="{{asset('js/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js')}}"></script>
+        <script type='text/javascript' src="{{asset('js/plugins/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>                
+        <script type='text/javascript' src="{{asset('js/plugins/bootstrap/bootstrap-datepicker.js')}}"></script>                
         <script type="text/javascript" src="{{asset('js/plugins/owl/owl.carousel.min.js')}}"></script>                 
         
         <script type="text/javascript" src="{{asset('js/plugins/moment.min.js')}}"></script>
